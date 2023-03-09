a4ae923d913b5287df61b6bf7741a05128773c08
The-Mushroom-That-Killed-Mario <andrejdedula@gmail.com>
2023-03-09 15:36:38 +0300

    Замененно использование LocalTime на stopWatch

--- src/main/java/com/noirix/aspect/MethodExecutionTimeAspect.java
+++ src/main/java/com/noirix/aspect/MethodExecutionTimeAspect.java
@@ -1,20 +1,15 @@
 package com.noirix.aspect;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-
-import java.time.Duration;
-import java.time.LocalTime;
+import org.springframework.util.StopWatch;
 
 @Component
 @Aspect
 public class MethodExecutionTimeAspect {
-
-    private LocalTime timeStart;
 
     //Срез точек наюлюдения(в общем случае шаблон, по которому аспект находит join Point's)
     @Pointcut("execution(* com.noirix.*.*.*.*(..))")
@@ -23,19 +18,20 @@ public class MethodExecutionTimeAspect {
 
     //Advice - набор инструкции среза
     //так же здесь указывается место действия этих инструкций(В аннотации @Around или похожей)
-    @Before("allMethodPointcut()")
-    public Object logAroundMethods(JoinPoint joinPoint) throws Throwable {
-        this.timeStart = LocalTime.now();
-        return joinPoint;
-    }
+    @Around("allMethodPointcut()")
+    public Object logAroundMethods(ProceedingJoinPoint joinPoint) throws Throwable {
 
-    @After("allMethodPointcut()")
-    public Object getCallMethodCount(JoinPoint joinPoint) {
-        Duration resultTime = Duration.between(timeStart, LocalTime.now());
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object object = joinPoint.proceed();
+
+        stopWatch.stop();
+
         System.out.println("Method {" + joinPoint.getSignature().getDeclaringType().getName() + "."
                 + joinPoint.getSignature().getName() + "()"
-                + "} working time: {" + resultTime.toMillis() + "}ms");
-        return joinPoint;
+                + "} working time: {" + stopWatch.getTotalTimeMillis() + "}ms");
+        return object;
     }
 
 }
